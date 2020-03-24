523c8a170553f9b6c9ff48c2a8410cbb594fef08
slukas111 <sashamati@gmail.com>
2020-03-24 14:55:03 -0700

    fixing error lower short

--- tests/test_echo.py
+++ tests/test_echo.py
@@ -9,17 +9,21 @@ import echo
 class TestEcho(unittest.TestCase):
 
     def setU(self):
-        pass
+        self.parser = echo.create_parser()
 
     def test_upper_short(self):
         # self.fail("You need to write this test")
         args = ["-u", "hello world"]
+        ns = self.parser.parse_args(args)
+        self.assertTrue(ns.upper)
         actual = echo.main(args)
         expected = "HELLO WORLD"
         self.assertEqual(actual, expected)
 
     def test_lower_short(self):
         args = ["-l", "HelLo WorLD"]
+        ns = self.parser.parse_args(args)
+        self.assertTrue(ns.lower)
         actual = echo.main(args)
         expected = "hello world"
         self.assertEqual(actual, expected)
@@ -31,7 +35,10 @@ class TestEcho(unittest.TestCase):
         self.assertEqual(actual, expected)
 
     def test_upper_long(self):
-        self.fail("You need to write this test")
+        args = ["--upper", "hello world"]
+        actual = echo.main(args)
+        expected = "HELLO WORLD"
+        self.assertEqual(actual, expected)
 
     def test_lower_long(self):
         self.fail("You need to write this test")
