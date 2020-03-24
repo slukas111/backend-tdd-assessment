7a9ac0b7d79d6eda1cacdcf99df0c5d1129cce9c
slukas111 <sashamati@gmail.com>
2020-03-24 12:57:43 -0700

    test_echo.py upper test

--- tests/test_echo.py
+++ tests/test_echo.py
@@ -4,11 +4,17 @@
 import unittest
 import echo
 
+
 # Your test case class goes here
 class TestEcho(unittest.TestCase):
-    
+
     def setU(self):
         pass
+
+    def test_upper_short(self):
+        self.fail("You need to write this test")
+
+        
 
 if __name__ == '__main__':
     unittest.main()
